06808eacbbaa8fbd4646eff39009af3f2fc583fd
denisdnepr66 <denisdenpr66@gmail.com>
2023-02-18 18:16:12 +0100

    initial commit  - Connected project to firebase  - Added design for start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireModule} from "@angular/fire/compat";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'novelpay-qr-demo',
+    appId: '1:770543068786:web:5f6eca5a8792283cb9c4eb',
+    storageBucket: 'novelpay-qr-demo.appspot.com',
+    apiKey: 'AIzaSyAp1ZLJbjrjkItVStJI8Q_ZinFy1x44wKM',
+    authDomain: 'novelpay-qr-demo.firebaseapp.com',
+    messagingSenderId: '770543068786',
+    measurementId: 'G-2N6BRYNXDE',
+  }
+}
