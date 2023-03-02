046ecd7506bed5fc5ca636328ef0ece58fe817f5
denisdnepr66 <denisdenpr66@gmail.com>
2023-03-02 14:27:55 +0100

    Added Pay again button

--- src/app/components/finish/finish.component.ts
+++ src/app/components/finish/finish.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FirebaseService} from "../../services/firebase.service";
 import {Guest} from "../../models/Guest";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Amount} from "../../models/Amount";
 
 @Component({
@@ -9,19 +9,19 @@ import {Amount} from "../../models/Amount";
     templateUrl: './finish.component.html',
     styleUrls: ['./finish.component.css']
 })
-export class FinishComponent {
+export class FinishComponent implements OnInit{
 
     guests: Guest[];
     paymentroom: string
     amount: Amount
-
     leftToPayRatio: number
     youPaidAmount: string
     youPaidRation: number
 
     constructor(
         private firebaseService: FirebaseService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private router: Router
     ) { }
 
     ngOnInit() {
@@ -36,5 +36,9 @@ export class FinishComponent {
             this.youPaidRation = parseFloat(this.youPaidAmount) / parseFloat(this.amount.amount) * 100
         })
 
+    }
+
+    onPayAgain() {
+        this.router.navigateByUrl('/start/' + this.paymentroom)
     }
 }
